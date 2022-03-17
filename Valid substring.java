b4ac9891792f1aeaf94aca8e0eadb6b3acdcfb66
sahana-deloitte <sahankh@deloitte.com>
2022-03-17 15:30:50 +0530

    Add files via upload

---
+++ Valid substring.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class PrintSubArrays {
+    public static void main(String[] args){
+        Scanner scan=new Scanner(System.in);
+        System.out.println("enter the number of input");
+        int a = scan.nextInt();
+        int arr[] = new int[a];
+        int count=0;
+        int i,j,sum=0;
+        for(i=0;i<a;i++)
+        {
+            arr[i]=scan.nextInt();
+        }
+        scan.close();
+        for(i=0;i<a;i++)
+        {
+            sum=0;
+            for(j=i;j<a;j++)
+            {
+               sum=arr[i]+sum;
+               System.out.println(sum);
+               if(sum<0){
+                   count++;
+               }
+            }
+        }
+        System.out.println(count);
+
+    }
+
+}
